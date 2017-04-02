d8d2e1335e44b5e8f617cc1ccd3926d65de2ed5e
oshaw <oscar.shaw@yahoo.com>
2017-04-02 10:36:19 -0700

    Major selection functional. Arrives at agreement now

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,26 @@
 'use strict'
 /* global XMLHttpRequest */
 
+let addMajors = function (origin, destination, year, majors) {
+  for (let major of majors) {
+    let button = document.createElement('p')
+    let click = function (element, origin, destination, year, major) {
+      element.addEventListener('click', function () {
+        let req = new XMLHttpRequest()
+        req.open('GET', '/?from=' + origin.path + '&to=' + destination.path + '&year=' + '16-17' + '&major=' + major.path, true)
+        req.onload = function () {
+          document.body.innerHTML = ''
+          console.log(req.responseText)
+        }
+        req.send()
+      })
+    }
+    button.textContent = major.name
+    button.style.display = 'block'
+    click(button, origin, destination, year, major)
+    document.body.appendChild(button)
+  }
+}
 let addYears = function (years) {
   for (let year of years) {
     let button = document.createElement('p')
@@ -15,9 +35,10 @@ let addDestinations = function (origin, destinations) {
     let click = function (element, origin, destination) {
       element.addEventListener('click', function () {
         let req = new XMLHttpRequest()
-        req.open('GET', '/?from=' + origin.path + '&to=' + destination.path, true)
+        req.open('GET', '/?from=' + origin.path + '&to=' + destination.path + '&year=' + '16-17', true)
         req.onload = function () {
           document.body.innerHTML = ''
+          addMajors(origin, destination, '16-17', JSON.parse(req.responseText))
         }
         req.send()
       })
