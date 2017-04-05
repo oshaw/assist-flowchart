5941823e04a66b29154183bbb33e57b23f5aabc0
Oscar Shaw <oshaw587@insite.4cd.edu>
2017-04-05 12:16:04 -0700

    Revert server.js from parser testing setup

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
 'use strict'
 
 const api = require('./api-handler.js')
-const parser = require('./parse-agreement.js')
 
 module.exports = function (server, root) {
-  console.log(parser())
-  // server.get('/api', function (req, res) {
-  //   api(req.query, function (data) { res.send(data) }, function (data) { res.send(data) })
-  // })
-  // server.get('*', function (req, res) {
-  //   if (req.query.endpoint !== undefined) {
-  //     api(req.query, function (data) { res.send(data) }, function () {})
-  //   } else res.sendFile(root + '/client/view.html')
-  // })
+  server.get('/api', function (req, res) {
+    api(req.query, function (data) { res.send(data) }, function (data) { res.send(data) })
+  })
+  server.get('*', function (req, res) {
+    if (req.query.endpoint !== undefined) {
+      api(req.query, function (data) { res.send(data) }, function () {})
+    } else res.sendFile(root + '/client/view.html')
+  })
 }
